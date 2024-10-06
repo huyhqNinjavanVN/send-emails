4bcd642795aef6262caf908771ae02f822cc1aed
quanghuyho06 <quanghuyho06@gmail.com>
2024-10-06 15:39:31 +0700

    new update

--- src/main.py
+++ src/main.py
@@ -1,7 +1,5 @@
 import smtplib
 from email import encoders
-import smtplib
-from email import encoders
 from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
